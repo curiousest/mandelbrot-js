460101969e7709e173d458e87330fc42557194d1
Douglas Hindson <dmhindson@gmail.com>
2014-09-18 10:35:07 +0800

    Refactored mandelbrot.js so that it can be used by other scripts to draw fractals.

--- mandelbrot.js
+++ mandelbrot.js
@@ -21,61 +21,21 @@
 /*
  * Global variables:
  */
-var zoomStart = 3.4;
-var zoom = [zoomStart, zoomStart];
-var lookAtDefault = [-0.6, 0];
-var lookAt = lookAtDefault;
-var xRange = [0, 0];
-var yRange = [0, 0];
-var escapeRadius = 10.0;
-var interiorColor = [0, 0, 0, 255];
-var reInitCanvas = true; // Whether to reload canvas size, etc
-var dragToZoom = true;
-var colors = [[0,0,0,0]];
 var renderId = 0; // To zoom before current render is finished
 
-/*
- * Initialize canvas
- */
-var canvas = $('canvasMandelbrot');
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
-//
-var ccanvas = $('canvasControls');
-ccanvas.width  = window.innerWidth;
-ccanvas.height = window.innerHeight;
-//
-var ctx = canvas.getContext('2d');
-var img = ctx.createImageData(canvas.width, 1);
 
 /*
  * Just a shorthand function: Fetch given element, jQuery-style
- */
-function $(id)
+ *
+function jquerystupidreplacement$(id)
 {
   return document.getElementById(id);
-}
+}*/
 
-function focusOnSubmit()
-{
-  var e = $('submitButton');
-  if ( e ) e.focus();
-}
-
-function getColorPicker()
-{
-  var p = $("colorScheme").value;
-  if ( p == "pickColorHSV1" ) return pickColorHSV1;
-  if ( p == "pickColorHSV2" ) return pickColorHSV2;
-  if ( p == "pickColorHSV3" ) return pickColorHSV3;
-  if ( p == "pickColorGrayscale2" ) return pickColorGrayscale2;
-  return pickColorGrayscale;
-}
 
 function getSamples()
 {
-  var i = parseInt($('superSamples').value, 10);
-  return i<=0? 1 : i;
+  return 1;
 }
 
 /*
@@ -100,7 +60,7 @@ function getSamples()
  * current pixel we're rendering, but K could be based on the "look at"
  * coordinate, or by letting the user select a point on the screen.
  */
-function iterateEquation(Cr, Ci, escapeRadius, iterations)
+function mandelbrotAlgorithm(Cr, Ci, escapeRadius, iterations)
 {
   var Zr = 0;
   var Zi = 0;
@@ -130,89 +90,10 @@ function iterateEquation(Cr, Ci, escapeRadius, iterations)
 }
 
 /*
- * Update URL's hash with render parameters so we can pass it around.
+ * Default Mandelbrot canvas size
  */
-function updateHashTag(samples, iterations)
-{
-  var radius = $('escapeRadius').value;
-  var scheme = $('colorScheme').value;
+var MANDELBROT_CANVAS_SIZE = {top_left: {x: -2, y: 1}, bottom_right: {x: 1.5, y: -1.5}};
 
-  location.hash = 'zoom=' + zoom + '&' +
-                  'lookAt=' + lookAt + '&' +
-                  'iterations=' + iterations + '&' +
-                  'superSamples=' + samples + '&' +
-                  'escapeRadius=' + radius + '&' +
-                  'colorScheme=' + scheme;
-}
-
-/*
- * Update small info box in lower right hand side
- */
-function updateInfoBox()
-{
-  // Update infobox
-  $('infoBox').innerHTML =
-    'x<sub>0</sub>=' + xRange[0] + ' y<sub>0</sub>=' + yRange[0] + ' ' +
-    'x<sub>1</sub>=' + xRange[1] + ' y<sub>1</sub>=' + yRange[1] + ' ' +
-    'w&#10799;h=' + canvas.width + 'x' + canvas.height + ' '
-        + (canvas.width*canvas.height/1000000.0).toFixed(1) + 'MP';
-}
-
-/*
- * Parse URL hash tag, returns whether we should redraw.
- */
-function readHashTag()
-{
-  var redraw = false;
-  var tags = location.hash.split('&');
-
-  for ( var i=0; i<tags.length; ++i ) {
-    var tag = tags[i].split('=');
-    var key = tag[0];
-    var val = tag[1];
-
-    switch ( key ) {
-      case '#zoom': {
-        var z = val.split(',');
-        zoom = [parseFloat(z[0]), parseFloat(z[1])];
-        redraw = true;
-      } break;
-
-      case 'lookAt': {
-        var l = val.split(',');
-        lookAt = [parseFloat(l[0]), parseFloat(l[1])];
-        redraw = true;
-      } break;
-
-      case 'iterations': {
-        $('steps').value = String(parseInt(val, 10));
-        $('autoIterations').checked = false;
-        redraw = true;
-      } break;
-
-      case 'escapeRadius': {
-        escapeRadius = parseFloat(val);
-        $('escapeRadius').value = String(escapeRadius);
-        redraw = true;
-      } break;
-
-      case 'superSamples': {
-        $('superSamples').value = String(parseInt(val, 10));
-        redraw = true;
-      } break;
-
-      case 'colorScheme': {
-        $('colorScheme').value = String(val);
-        redraw = true;
-      } break;
-    }
-  }
-
-  if ( redraw )
-    reInitCanvas = true;
-
-  return redraw;
-}
 
 /*
  * Return number with metric units
@@ -263,17 +144,17 @@ function hsv_to_rgb(h, s, v)
  */
 function adjustAspectRatio(xRange, yRange, canvas)
 {
-  var ratio = Math.abs(xRange[1]-xRange[0]) / Math.abs(yRange[1]-yRange[0]);
+  var ratio = Math.abs(xRange.y-xRange.x) / Math.abs(yRange.y-yRange.x);
   var sratio = canvas.width/canvas.height;
   if ( sratio>ratio ) {
     var xf = sratio/ratio;
-    xRange[0] *= xf;
-    xRange[1] *= xf;
+    xRange.x *= xf;
+    xRange.y *= xf;
       zoom[0] *= xf;
   } else {
     var yf = ratio/sratio;
-    yRange[0] *= yf;
-    yRange[1] *= yf;
+    yRange.x *= yf;
+    yRange.y *= yf;
       zoom[1] *= yf;
   }
 }
@@ -299,50 +180,30 @@ function divRGB(v, div)
 /*
  * Render the Mandelbrot set
  */
-function draw(pickColor, superSamples)
+function draw(canvas, xRange, yRange, pickColor, fractalAlgorithm)
 {
-  if ( lookAt === null ) lookAt = [-0.6, 0];
-  if ( zoom === null ) zoom = [zoomStart, zoomStart];
-
-  xRange = [lookAt[0]-zoom[0]/2, lookAt[0]+zoom[0]/2];
-  yRange = [lookAt[1]-zoom[1]/2, lookAt[1]+zoom[1]/2];
-
-  if ( reInitCanvas ) {
-    reInitCanvas = false;
-
-    canvas = $('canvasMandelbrot');
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    ccanvas = $('canvasControls');
-    ccanvas.width  = window.innerWidth;
-    ccanvas.height = window.innerHeight;
-
-    ctx = canvas.getContext('2d');
-    img = ctx.createImageData(canvas.width, 1);
-
-    adjustAspectRatio(xRange, yRange, canvas);
+  if (!canvas || !xRange || !yRange || !pickColor || !fractalAlgorithm){
+    return 0;
   }
 
-  var steps = parseInt($('steps').value, 10);
+  var ctx = canvas.getContext('2d');
+  var img = ctx.createImageData(canvas.width, 1);
+  
+  var superSamples = getSamples();
 
-  if ( $('autoIterations').checked ) {
-    var f = Math.sqrt(
-            0.001+2.0 * Math.min(
-              Math.abs(xRange[0]-xRange[1]),
-              Math.abs(yRange[0]-yRange[1])));
+  //adjustAspectRatio(xRange, yRange, canvas);
 
-    steps = Math.floor(223.0/f);
-    $('steps').value = String(steps);
-  }
+  var f = Math.sqrt(
+    0.001+2.0 * Math.min(
+      Math.abs(xRange.x-xRange.y),
+      Math.abs(yRange.x-yRange.y)));
 
-  var escapeRadius = Math.pow(parseFloat($('escapeRadius').value), 2.0);
-  var dx = (xRange[1] - xRange[0]) / (0.5 + (canvas.width-1));
-  var dy = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
-  var Ci_step = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
+  steps = Math.floor(223.0/f);
 
-  updateHashTag(superSamples, steps);
-  updateInfoBox();
+  var escapeRadius = Math.pow(parseFloat(10.0), 2.0);
+  var dx = (xRange.y - xRange.x) / (0.5 + (canvas.width-1));
+  var dy = (yRange.y - yRange.x) / (0.5 + (canvas.height-1));
+  var Ci_step = (yRange.y - yRange.x) / (0.5 + (canvas.height-1));
 
   // Only enable one render at a time
   renderId += 1;
@@ -357,7 +218,7 @@ function draw(pickColor, superSamples)
       for ( var s=0; s<superSamples; ++s ) {
         var rx = Math.random()*Cr_step;
         var ry = Math.random()*Ci_step;
-        var p = iterateEquation(Cr - rx/2, Ci - ry/2, escapeRadius, steps);
+        var p = fractalAlgorithm(Cr - rx/2, Ci - ry/2, escapeRadius, steps);
         color = addRGB(color, pickColor(steps, p[0], p[1], p[2]));
       }
 
@@ -375,7 +236,7 @@ function draw(pickColor, superSamples)
     var Cr = Cr_init;
 
     for ( var x=0; x<canvas.width; ++x, Cr += Cr_step ) {
-      var p = iterateEquation(Cr, Ci, escapeRadius, steps);
+      var p = fractalAlgorithm(Cr, Ci, escapeRadius, steps);
       var color = pickColor(steps, p[0], p[1], p[2]);
       img.data[off++] = color[0];
       img.data[off++] = color[1];
@@ -402,9 +263,9 @@ function draw(pickColor, superSamples)
     var startHeight = canvas.height;
     var startWidth = canvas.width;
     var lastUpdate = start;
-    var updateTimeout = parseFloat($('updateTimeout').value);
+    var updateTimeout = 200;
     var pixels = 0;
-    var Ci = yRange[0];
+    var Ci = yRange.x;
     var sy = 0;
     var drawLineFunc = superSamples>1? drawLineSuperSampled : drawLine;
     var ourRenderId = renderId;
@@ -419,7 +280,7 @@ function draw(pickColor, superSamples)
         return;
       }
 
-      drawLineFunc(Ci, 0, xRange[0], dx);
+      drawLineFunc(Ci, 0, xRange.x, dx);
       Ci += Ci_step;
       pixels += canvas.width;
       ctx.putImageData(img, 0, sy);
@@ -488,6 +349,8 @@ function smoothColor(steps, n, Tr, Ti)
   return 5 + n - logHalfBase - Math.log(Math.log(Tr+Ti))*logBase;
 }
 
+var interiorColor = [0, 0, 0, 255];
+
 function pickColorHSV1(steps, n, Tr, Ti)
 {
   if ( n == steps ) // converged?
@@ -549,186 +412,3 @@ function pickColorGrayscale2(steps, n, Tr, Ti)
 
   return pickColorGrayscale(steps, n, Tr, Ti);
 }
-
-function main()
-{
-  $('viewPNG').onclick = function(event)
-  {
-    window.location = canvas.toDataURL('image/png');
-  };
-
-  $('steps').onkeypress = function(event)
-  {
-    // disable auto-iterations when user edits it manually
-    $('autoIterations').checked = false;
-  }
-
-  $('resetButton').onclick = function(even)
-  {
-    $('settingsForm').reset();
-    setTimeout(function() { location.hash = ''; }, 1);
-    zoom = [zoomStart, zoomStart];
-    lookAt = lookAtDefault;
-    reInitCanvas = true;
-    draw(getColorPicker(), getSamples());
-  };
-
-  if ( dragToZoom == true ) {
-    var box = null;
-
-    $('canvasControls').onmousedown = function(e)
-    {
-      if ( box == null )
-        box = [e.clientX, e.clientY, 0, 0];
-    }
-
-    $('canvasControls').onmousemove = function(e)
-    {
-      if ( box != null ) {
-        var c = ccanvas.getContext('2d');
-        c.lineWidth = 1;
-
-        // clear out old box first
-        c.clearRect(0, 0, ccanvas.width, ccanvas.height);
-
-        // draw new box
-        c.strokeStyle = '#FF3B03';
-        box[2] = e.clientX;
-        box[3] = e.clientY;
-        c.strokeRect(box[0], box[1], box[2]-box[0], box[3]-box[1]);
-      }
-    }
-
-    var zoomOut = function(event) {
-      var x = event.clientX;
-      var y = event.clientY;
-
-      var w = window.innerWidth;
-      var h = window.innerHeight;
-
-      var dx = (xRange[1] - xRange[0]) / (0.5 + (canvas.width-1));
-      var dy = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
-
-      x = xRange[0] + x*dx;
-      y = yRange[0] + y*dy;
-
-      lookAt = [x, y];
-
-      if ( event.shiftKey ) {
-        zoom[0] /= 0.5;
-        zoom[1] /= 0.5;
-      }
-
-      draw(getColorPicker(), getSamples());
-    };
-
-    $('canvasControls').onmouseup = function(e)
-    {
-      if ( box != null ) {
-        // Zoom out?
-        if ( e.shiftKey ) {
-          box = null;
-          zoomOut(e);
-          return;
-        }
-
-        /*
-         * Cleaer entire canvas
-         */
-        var c = ccanvas.getContext('2d');
-        c.clearRect(0, 0, ccanvas.width, ccanvas.height);
-
-        /*
-         * Calculate new rectangle to render
-         */
-        var x = Math.min(box[0], box[2]) + Math.abs(box[0] - box[2]) / 2.0;
-        var y = Math.min(box[1], box[3]) + Math.abs(box[1] - box[3]) / 2.0;
-
-        var dx = (xRange[1] - xRange[0]) / (0.5 + (canvas.width-1));
-        var dy = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
-
-        x = xRange[0] + x*dx;
-        y = yRange[0] + y*dy;
-
-        lookAt = [x, y];
-
-        /*
-         * This whole code is such a mess ...
-         */
-
-        var xf = Math.abs(Math.abs(box[0]-box[2])/canvas.width);
-        var yf = Math.abs(Math.abs(box[1]-box[3])/canvas.height);
-
-        zoom[0] *= Math.max(xf, yf); // retain aspect ratio
-        zoom[1] *= Math.max(xf, yf);
-
-        box = null;
-        draw(getColorPicker(), getSamples());
-      }
-    }
-  }
-
-  /*
-   * Enable zooming (currently, the zooming is inexact!) Click to zoom;
-   * perfect to mobile phones, etc.
-   */
-  if ( dragToZoom == false ) {
-    $('canvasMandelbrot').onclick = function(event)
-    {
-      var x = event.clientX;
-      var y = event.clientY;
-      var w = window.innerWidth;
-      var h = window.innerHeight;
-
-      var dx = (xRange[1] - xRange[0]) / (0.5 + (canvas.width-1));
-      var dy = (yRange[1] - yRange[0]) / (0.5 + (canvas.height-1));
-
-      x = xRange[0] + x*dx;
-      y = yRange[0] + y*dy;
-
-      lookAt = [x, y];
-
-      if ( event.shiftKey ) {
-        zoom[0] /= 0.5;
-        zoom[1] /= 0.5;
-      } else {
-        zoom[0] *= 0.5;
-        zoom[1] *= 0.5;
-      }
-
-      draw(getColorPicker(), getSamples());
-    };
-  }
-
-  /*
-   * When resizing the window, be sure to update all the canvas stuff.
-   */
-  window.onresize = function(event)
-  {
-    reInitCanvas = true;
-  };
-
-  /*
-   * Read hash tag and render away at page load.
-   */
-  readHashTag();
-
-  /*
-   * This is the weirdest bug ever.  When I go directly to a link like
-   *
-   *   mandelbrot.html#zoom=0.01570294345468629,0.010827482681521361&
-   *   lookAt=-0.3083866260309053,-0.6223590662533901&iterations=5000&
-   *   superSamples=1&escapeRadius=16&colorScheme=pickColorHSV2
-   *
-   * it will render a black image, but if I call the function twice, it
-   * works nicely.  Must be a global variable that's not been set upon the
-   * first entry to the function (TODO: Find out what's wrong).
-   *
-   * Yeah, I know, the code is a total mess at the moment.  I'll get back
-   * to that.
-   */
-  draw(getColorPicker(), getSamples());
-  draw(getColorPicker(), getSamples());
-}
-
-main();
